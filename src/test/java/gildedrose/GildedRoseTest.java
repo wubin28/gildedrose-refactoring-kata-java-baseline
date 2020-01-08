25d135cbe0405ecf28df8132a4b78bba1ac6125a
benwu <wubinben@icloud.com>
2020-01-08 23:44:08 +0800

    make the tests stronger

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -13,7 +13,7 @@ public class GildedRoseTest {
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdateQuality,
                 new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
-                new Integer[]{-1, 0, 11},
+                new Integer[]{-1, 0, 1, 6, 11},
                 new Integer[]{0, 1, 49, 50}
         );
     }
