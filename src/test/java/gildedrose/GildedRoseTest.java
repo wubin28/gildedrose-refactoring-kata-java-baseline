e1d11c55545b52ef3e5f75489a4bfcb6364bad91
benwu <wubinben@icloud.com>
2020-01-08 22:28:36 +0800

    approvals verify item.toString()

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.assertEquals;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void updateQuality() {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         // assertEquals("foo", app.items[0].name);
-        Approvals.verify(app.items[0].name);
+        Approvals.verify(app.items[0].toString());
     }
 
 }
