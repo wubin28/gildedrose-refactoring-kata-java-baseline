27e19795cb727b65a1b16ceee888826e333deda0
benwu <wubinben@icloud.com>
2020-01-09 23:25:18 +0800

    inline method for AgedBrie

--- src/main/java/gildedrose/AgedBrie.java
+++ src/main/java/gildedrose/AgedBrie.java
@@ -7,6 +7,59 @@ public class AgedBrie extends Item {
 
     @Override
     void doUpdateQuality() {
-        super.doUpdateQuality();
+        switch (name) {
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality = quality + 1;
+
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    quality = quality - quality;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+
+                break;
+            default:
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality > 0) {
+                        quality = quality - 1;
+                    }
+                }
+                break;
+        }
     }
 }
