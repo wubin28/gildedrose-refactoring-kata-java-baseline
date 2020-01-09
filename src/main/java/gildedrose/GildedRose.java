2b818eb053d9873e9570b01d69742561bef8e44b
benwu <wubinben@icloud.com>
2020-01-09 22:51:49 +0800

    move instance method from GildedRose to Item

--- src/main/java/gildedrose/GildedRose.java
+++ src/main/java/gildedrose/GildedRose.java
@@ -10,64 +10,8 @@ public class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            doUpdateQuality(item);
+            item.doUpdateQuality();
         }
     }
 
-    private void doUpdateQuality(Item item) {
-        switch (item.name) {
-            case "Aged Brie":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
-                }
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-
-                break;
-            default:
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-                break;
-        }
-    }
 }
--- src/main/java/gildedrose/Item.java
+++ src/main/java/gildedrose/Item.java
@@ -18,4 +18,61 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    void doUpdateQuality() {
+        switch (name) {
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality = quality + 1;
+
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    quality = quality - quality;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+
+                break;
+            default:
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality > 0) {
+                        quality = quality - 1;
+                    }
+                }
+                break;
+        }
+    }
 }
