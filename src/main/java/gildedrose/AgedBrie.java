f7101801ef934740d719505e2180fb2dc6d46de3
benwu <wubinben@icloud.com>
2020-01-09 23:31:39 +0800

    remove useless statements for AgedBrie

--- src/main/java/gildedrose/AgedBrie.java
+++ src/main/java/gildedrose/AgedBrie.java
@@ -7,52 +7,15 @@ public class AgedBrie extends Item {
 
     @Override
     void doUpdateQuality() {
-        if (true) {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
             if (quality < 50) {
                 quality = quality + 1;
-            }
-
-            sellIn = sellIn - 1;
-
-            if (sellIn < 0) {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
-        } else if (false) {
-            if (quality < 50) {
-                quality = quality + 1;
-
-                if (sellIn < 11) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-
-                if (sellIn < 6) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-            }
-
-            sellIn = sellIn - 1;
-
-            if (sellIn < 0) {
-                quality = quality - quality;
-            }
-        } else if (false) {
-        } else {
-            if (quality > 0) {
-                quality = quality - 1;
-            }
-
-            sellIn = sellIn - 1;
-
-            if (sellIn < 0) {
-                if (quality > 0) {
-                    quality = quality - 1;
-                }
             }
         }
     }
