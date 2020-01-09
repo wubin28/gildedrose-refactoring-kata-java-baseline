03223277c4e4acfc8a5dfbf39861b16062399d00
benwu <wubinben@icloud.com>
2020-01-09 23:41:28 +0800

    remove useless statements for BackstagePass

--- src/main/java/gildedrose/BackstagePass.java
+++ src/main/java/gildedrose/BackstagePass.java
@@ -4,4 +4,29 @@ public class BackstagePass extends Item {
     public BackstagePass(int sellIn, int quality) {
         super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
+
+    @Override
+    void doUpdateQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+
+            if (sellIn < 11) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+            quality = quality - quality;
+        }
+    }
 }
