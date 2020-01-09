d9e7d71b47394663ce069e43f2760f09ecd4475f
benwu <wubinben@icloud.com>
2020-01-09 23:27:18 +0800

    replace switch with if

--- src/main/java/gildedrose/AgedBrie.java
+++ src/main/java/gildedrose/AgedBrie.java
@@ -7,59 +7,53 @@ public class AgedBrie extends Item {
 
     @Override
     void doUpdateQuality() {
-        switch (name) {
-            case "Aged Brie":
+        if ("Aged Brie".equals(name)) {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+
+            sellIn = sellIn - 1;
+
+            if (sellIn < 0) {
                 if (quality < 50) {
                     quality = quality + 1;
                 }
+            }
+        } else if ("Backstage passes to a TAFKAL80ETC concert".equals(name)) {
+            if (quality < 50) {
+                quality = quality + 1;
 
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0) {
+                if (sellIn < 11) {
                     if (quality < 50) {
                         quality = quality + 1;
                     }
                 }
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (quality < 50) {
-                    quality = quality + 1;
 
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
+                if (sellIn < 6) {
+                    if (quality < 50) {
+                        quality = quality + 1;
                     }
                 }
+            }
 
-                sellIn = sellIn - 1;
+            sellIn = sellIn - 1;
 
-                if (sellIn < 0) {
-                    quality = quality - quality;
-                }
-                break;
-            case "Sulfuras, Hand of Ragnaros":
+            if (sellIn < 0) {
+                quality = quality - quality;
+            }
+        } else if ("Sulfuras, Hand of Ragnaros".equals(name)) {
+        } else {
+            if (quality > 0) {
+                quality = quality - 1;
+            }
 
-                break;
-            default:
+            sellIn = sellIn - 1;
+
+            if (sellIn < 0) {
                 if (quality > 0) {
                     quality = quality - 1;
                 }
-
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0) {
-                    if (quality > 0) {
-                        quality = quality - 1;
-                    }
-                }
-                break;
+            }
         }
     }
 }
