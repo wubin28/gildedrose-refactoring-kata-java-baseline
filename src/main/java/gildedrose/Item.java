a152ab116a1af797aff0165138f20e4af7719ad3
benwu <wubinben@icloud.com>
2020-01-09 23:47:51 +0800

    remove useless statements for Item

--- src/main/java/gildedrose/Item.java
+++ src/main/java/gildedrose/Item.java
@@ -33,59 +33,16 @@ public class Item {
     }
 
     void doUpdateQuality() {
-        switch (name) {
-            case "Aged Brie":
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
+        if (quality > 0) {
+            quality = quality - 1;
+        }
 
-                sellIn = sellIn - 1;
+        sellIn = sellIn - 1;
 
-                if (sellIn < 0) {
-                    if (quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (quality < 50) {
-                    quality = quality + 1;
-
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0) {
-                    quality = quality - quality;
-                }
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-
-                break;
-            default:
-                if (quality > 0) {
-                    quality = quality - 1;
-                }
-
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0) {
-                    if (quality > 0) {
-                        quality = quality - 1;
-                    }
-                }
-                break;
+        if (sellIn < 0) {
+            if (quality > 0) {
+                quality = quality - 1;
+            }
         }
     }
 }
